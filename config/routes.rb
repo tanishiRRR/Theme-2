aa34f2f6e8f778a993151c14681cc4ae3ec3ab7a
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-14 16:21:23 +0000

    [Update]アプリケーションに必要なファイルの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
-  get 'lists/new'
-  get 'lists/show'
-  get 'lists/index'
-  get 'lists/edit'
+  get 'books/new'
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
