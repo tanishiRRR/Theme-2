51ee21b44d89d346bf048b4af4242dc1860f2430
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-16 15:30:57 +0000

    [Update]bookのindex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,22 @@
 class BooksController < ApplicationController
   def books
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'top' => 'homes#top'
-  get 'books' =>'books#books'
+  get 'books' => 'books#books'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
