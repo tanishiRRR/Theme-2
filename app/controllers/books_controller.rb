ba503f804cc0741d03041b41b447588ad953ee4a
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-18 15:38:51 +0000

    [update]booksのerrorメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,17 @@
 class BooksController < ApplicationController
-  def books
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
